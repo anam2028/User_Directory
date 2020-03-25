2e6964f5c494f4044236edcfaaa67634b8cdee65
Anamika Panna <anamika2028@gmail.com>
2020-03-25 18:31:02 -0400

    created app with employees data in state

--- App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/employee";
-
-const App = () => <employee />;
-
-export default App;
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import employees from "./employees.json";
+
+class App extends React.Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    employees
+  };
+
+render(){
+    return(<div></div>)
+}
+}
+
+export default App;
